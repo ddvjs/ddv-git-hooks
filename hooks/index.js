cccf093897461d0b481caab895d246593745e387
hua <yuchonghua@163.com>
2017-06-22 15:27:00 +0800

    fixbug

--- hooks/index.js
+++ hooks/index.js
@@ -1,25 +1,42 @@
 'use strict'
 const hooksFn = Object.create(null)
 module.exports = hooks
+const path = require('path')
 
 function hooks (type, event) {
-  if (!hooksFn[type]) {
-    loadHooksFn(type)
-    .catch(e => console.error('没有type这个类型', e))
-  }
-  if (!hooksFn[type]) {
-    console.error('没有type这个类型')
-    return
-  }
-  hooksFn[type](event)
+  loadHooksFn(type, event.config)
+  .then(fn => {
+    fn(event)
+  })
+  .catch(e => console.error('运行失败', e))
 }
-function loadHooksFn (type) {
-  return new Promise(function (resolve, reject) {
-    try {
-      hooksFn[type] = require(type)
-      resolve(hooksFn[type])
-    } catch (e) {
-      reject(e)
+function loadHooksFn (type, config) {
+  if (hooksFn[type]) {
+    return Promise.resolve(hooksFn[type])
+  }
+  return Promise.resolve(config.hooksDir)
+  .then(hooksDir => {
+    if (config.hooksDir) {
+      return new Promise(function (resolve, reject) {
+        try {
+          hooksFn[type] = require(path.join(hooksDir, type))
+          resolve(hooksFn[type])
+        } catch (e) {
+          reject(e)
+        }
+      })
+    } else {
+      return Promise.reject(new Error('not hooksDir'))
     }
+  })
+  .catch(e => {
+    return new Promise(function (resolve, reject) {
+      try {
+        hooksFn[type] = require(type)
+        resolve(hooksFn[type])
+      } catch (e) {
+        reject(e)
+      }
+    })
   })
 }
