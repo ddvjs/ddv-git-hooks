15f264aa0cb61676d3ec7627b13f1ab8bc7103f8
hua <yuchonghua@163.com>
2017-06-27 22:06:09 +0800

    fixbug

--- server/execImplement.js
+++ server/execImplement.js
@@ -1,21 +1,28 @@
 module.exports = getRemoteByRemoteUrl
 const fs = require('fs')
-const exec = require('child_process').exec
+const spawn = require('child_process').spawn
 const filesType = {
   'pull': 'pull.ddv-gitlab-hooks.sh',
   'clone': 'clone.ddv-gitlab-hooks.sh',
   'pr': 'pr.ddv-gitlab-hooks.sh'
 }
 
-function getRemoteByRemoteUrl (path, type) {
+function getRemoteByRemoteUrl (cwd, type) {
   return new Promise(function (resolve, reject) {
-    let filePath = `${path}/${filesType[type]}`
+    let filePath = `${cwd}/${filesType[type]}`
 
     if (fs.existsSync(filePath) && filesType[type]) {
-      exec(filePath, (err, stdout, stderr) => {
-        console.log(`stdout: ${stdout}`)
-        console.error(`stderr: ${stderr}`)
-        err ? reject(err) : resolve()
+      let sh = spawn(filePath, {cwd})
+      sh.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`)
+      })
+
+      sh.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`)
+      })
+
+      sh.on('close', (code) => {
+        code === 0 ? resolve() : reject(new Error(`child process exited with code ${code}`))
       })
     } else {
       var e = new Error(`the corresponding file was not found: ${filePath}`)
